6a38f916add3a8eb4ae402d9181e9a6613f9d2a0
Carlos <ixarlos@gmail.com>
2018-05-25 15:25:57 -0500

    -

--- clase2/src/App.js
+++ clase2/src/App.js
@@ -19,13 +19,10 @@ class App extends Component {
     .then(response => response.json())
     .then(users => {
         console.log(users);
-
-        this.setState (
-        {
+        this.setState({
           users,
           isLoading: false,
-        }
-      )
+        })
       }).catch(error => {
         console.log(error);
       });
--- clase2/src/components/GeolocationPresentational.js
+++ clase2/src/components/GeolocationPresentational.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const GeolocationPresentational = ({lat, lng}) => {
-  return <h1> Esta es mi posición: latitud {lat} longitud {lng}</h1>
+const GeolocationPresentational = ({lat , lng}) => {
+  return <h1> Esta es mi posición: latitud { lat } longitud { lng } </h1>
 }
 
 export default GeolocationPresentational
